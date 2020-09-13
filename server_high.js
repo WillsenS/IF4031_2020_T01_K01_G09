01566ec03dbb403278df666067ed59b512348c20
Eka Novendra Wahyunadi <13517011@std.stei.itb.ac.id>
2020-09-13 21:46:22 +0700

    Add argument to server high

--- server_high.js
+++ server_high.js
@@ -1,30 +1,19 @@
 const http = require('http');
 const fs = require('fs');
 
-console.log('[INFO] Starting server on port 8080...')
+if (process.argv.length <= 2) {
+    console.log("Please provide PORT and FILE (example : 'node server_high.js 8080 dummy1.html')")
+    return;
+}
 
+const argument = process.argv.slice(2);
+console.log('[INFO] Starting server on port ' + argument[0]);
 http.createServer(function (req, res) {
-    switch(req.url){
-        case '/dummy1':
-            fs.readFile('dummy_file/dummy1.html', (err, data) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                return res.end();
-            });
-            break;
-        case '/dummy2':
-            fs.readFile('dummy_file/dummy2.html', (err, data) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                return res.end();
-            });
-            break;
-        default:
-            
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('Please choose between /dummy1 and /dummy 2 ..!');
-            return res.end();
-    }
-}).listen(8080);
+    fs.readFile(argument[1], (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(argument[0]);
 
-console.log('[INFO] Server running on port 8080')
+console.log('[INFO] Server running on port ' + argument[0]);
